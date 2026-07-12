baf65c750a5f4f2f68614183da52219212cc6733
TanishkaMishraa <tanishka.ns845@gmail.com>
2026-07-12 12:03:58 +0530

    Update service layer for asset, booking, auth and maintenance modules

--- client/src/services/assetService.js
+++ client/src/services/assetService.js
@@ -1,1 +1,34 @@
-// Asset API calls belong here.
+import api from "./api";
+
+export const getAssets = async (params = {}) => {
+  const response = await api.get("/assets", { params });
+  return response.data;
+};
+
+export const getAssetById = async (id) => {
+  const response = await api.get(`/assets/${id}`);
+  return response.data;
+};
+
+export const createAsset = async (payload) => {
+  const response = await api.post("/assets", payload);
+  return response.data;
+};
+
+export const updateAsset = async (id, payload) => {
+  const response = await api.put(`/assets/${id}`, payload);
+  return response.data;
+};
+
+export const deleteAsset = async (id) => {
+  const response = await api.delete(`/assets/${id}`);
+  return response.data;
+};
+
+export default {
+  getAssets,
+  getAssetById,
+  createAsset,
+  updateAsset,
+  deleteAsset,
+};
